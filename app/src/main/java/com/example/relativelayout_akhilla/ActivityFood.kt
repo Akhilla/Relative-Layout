55c169971643db8dadfd8fbeac5129f878e5bbee
Akhilla <muhammadakhillaghautsyi@gmail.com>
2023-02-13 15:36:54 +0700

    belajar relative layout

---
+++ app/src/main/java/com/example/relativelayout_akhilla/ActivityFood.kt
@@ -0,0 +1,11 @@
+package com.example.relativelayout_akhilla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+    }
+}
---
+++ app/src/main/java/com/example/relativelayout_akhilla/networking/Api.kt
@@ -0,0 +1,5 @@
+package com.example.relativelayout_akhilla.networking
+
+object Api {
+    var Categories = " https://gist.github.com/MasKoding/8cc351fa3a90aeac171fc4e5f18f47d3"
+}

93ae3f4e43d3ff34ded0b55e16cffc49dfabd4d2
Akhilla <muhammadakhillaghautsyi@gmail.com>
2023-02-14 10:15:33 +0700

    menambahkan android networking untuk consume API

--- app/src/main/java/com/example/relativelayout_akhilla/ActivityFood.kt
+++ app/src/main/java/com/example/relativelayout_akhilla/ActivityFood.kt
@@ -2,10 +2,77 @@ package com.example.relativelayout_akhilla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.example.relativelayout_akhilla.adapter.CategoriesAdapter
+import com.example.relativelayout_akhilla.model.CategoriesModel
+import com.example.relativelayout_akhilla.networking.Api
+import org.json.JSONException
+import org.json.JSONObject
 
 class ActivityFood : AppCompatActivity() {
+
+    lateinit var categoriesAdapter: CategoriesAdapter
+    lateinit var categoriesModel: CategoriesModel
+    lateinit var categoryList: ArrayList<CategoriesModel>
+    lateinit var rvCategories:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
+        categoryList = ArrayList()
+        rvCategories=findViewById(R.id.rv_food)
+        var layoutManager = GridLayoutManager(this, 2, RecyclerView.VERTICAL,false)
+    }
+
+    private val categories: Unit
+        get(){
+            AndroidNetworking.get(Api.Categories)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener{
+                    override fun onResponse(response: JSONObject?) {
+
+                        try {
+                            var foodArray = response?.getJSONObject("categories")
+                            for (i in 0 until foodArray!!.length()){
+                                var temp = foodArray.getJSONObject(i)
+                                var dataApi = CategoriesModel()
+                                dataApi.strCategory = temp.getString("strCategory")
+                                dataApi.strCategoryThumb = temp.getString("strCategoryThumb")
+                                dataApi.strCategoryDescription = temp.getString("strCategoryDescription")
+
+                                categoryList.add(dataApi)
+                                showCategories()
+                            }
+
+                        }
+                        catch (e:JSONException){
+                            e.printStackTrace()
+                            Toast.makeText(this@ActivityFood, "object not found",
+                            Toast.LENGTH_SHORT
+                                ).show()
+                        }
+
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        Toast.makeText( this@ActivityFood,"close connection" ,
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+                )
+        }
+
+    private fun showCategories() {
+       categoriesAdapter = CategoriesAdapter(this,categoryList)
+        rvCategories.adapter = categoriesAdapter
     }
 }
--- app/src/main/java/com/example/relativelayout_akhilla/networking/Api.kt
+++ app/src/main/java/com/example/relativelayout_akhilla/networking/Api.kt
@@ -1,5 +1,5 @@
 package com.example.relativelayout_akhilla.networking
 
 object Api {
-    var Categories = " https://gist.github.com/MasKoding/8cc351fa3a90aeac171fc4e5f18f47d3"
+    var Categories = " www.themealdb.com/api/json/v1/1/categories.php"
 }
